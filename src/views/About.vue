a321f68f8d20120d54fe637834605cfd3d73ac2a
luminous <pjspan@foxmail.com>
2022-07-01 15:24:57 +0800

    init index.html

--- src/views/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h1 @click="$store.commit('add')">{{$store.state.counter}}</h1>
-    <h1 @click="$store.dispatch('add')">{{$store.state.counter}}</h1>
-  </div>
-</template>
