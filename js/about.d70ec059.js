491875b6824e152ac9bc9a662c628e2523287c24
luminous <pjspan@foxmail.com>
2022-07-14 22:06:16 +0800

    try vue3

---
+++ js/about.d70ec059.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdemo"]=self["webpackChunkdemo"]||[]).push([[443],{4335:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(3396);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var o=u(89);const i={},d=(0,o.Z)(i,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=about.d70ec059.js.map
