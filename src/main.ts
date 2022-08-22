5db703229376a0c7b4f1ccb0dbb7bf15dbf0bf26
luminous <pjspan@foxmail.com>
2022-08-22 23:16:00 +0800

    init highlightjs

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,16 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+
+//引入highlight.js
+import 'highlight.js/styles/atom-one-dark.css'
+import 'highlight.js/lib/common'
+import hljs from '@highlightjs/vue-plugin'
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(hljs)
+
+app.mount("#app");
+// createApp(App).use(store).use(router).mount("#app");
--- src/pages/about/About.vue
+++ src/pages/about/About.vue
@@ -1,3 +1,13 @@
 <template>
     <div class="about">关于</div>
+  <div>
+    let a = 10
+  </div>
+    <code lang="javascript" mode="dark">
+
+      let a = 10
+    </code>
+  <highlightjs code="const a = 10;
+function add(){}
+"></highlightjs>
 </template>
