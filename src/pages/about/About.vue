c6b13561c134697794e807aca4c36bea63067fa8
luminous <pjspan@foxmail.com>
2022-08-23 21:28:37 +0800

    调整ui

--- src/pages/about/About.vue
+++ src/pages/about/About.vue
@@ -9,29 +9,32 @@
         目前就职于上海，热爱着且永远热爱自己，该博客用于记录生活，会是一个不错的宝藏网址。
       </p>
     </div>
-    <div>
-      <a href="https://github.com/luminous1011" target="_blank">
-        <img
-          src="https://ghchart.rshah.org/419c58/luminous1011"
-          alt=""
-          id="githubChart"
-        />
-      </a>
-    </div>
+    <a :href="GITHUB_CONTACT_URL" target="_blank">
+      <img
+        id="githubChart"
+        :src="GITHUB_CHART_URL"
+      />
+    </a>
     <div class="about_contact clearfix">
-      <a href="http://wpa.qq.com/msgrd?v=3&uin=1980393754&site=qq&menu=yes" target="_blank">
+      <a
+        :href="QQ_CONTACT_URL"
+        target="_blank"
+      >
         <div class="qq">
           <span class="content">1980393754</span>
           <i class="fa fa-qq"></i>
         </div>
       </a>
-       <a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=aRkDGhkIBykPBhEECAAFRwoGBA" target="_blank">
+      <a
+        :href="MAIL_CONTACT_URL"
+        target="_blank"
+      >
         <div class="email">
           <span class="content">pjspan@foxmail.com</span>
           <i class="fa fa-envelope-o"></i>
         </div>
       </a>
-       <a  target="_blank">
+      <a target="_blank">
         <div class="wechat">
           <span class="content">luminous_chat</span>
           <i class="fa fa-wechat"></i>
@@ -44,78 +47,21 @@
     </div>
   </div>
 </template>
+
 <script lang="ts" setup>
+const GITHUB_CONTACT_URL="https://github.com/luminous1011"
+const GITHUB_CHART_URL="https://ghchart.rshah.org/419c58/luminous1011"
+const QQ_CONTACT_URL="http://wpa.qq.com/msgrd?v=3&uin=1980393754&site=qq&menu=yes"
+const MAIL_CONTACT_URL="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=aRkDGhkIBykPBhEECAAFRwoGBA"
 import Meta from "@/components/meta/Meta.vue";
 import Title from "@/components/title/Title.vue";
+
+
+
 
 const meta = { date: "2022-07-20", comments: 8, view: 122 };
 </script>
 
 <style lang="less">
-.about {
-  padding: 60px 25px 20px;
-  p {
-    margin: 20px 0;
-    line-height: 2;
-    vertical-align: baseline;
-  }
-  #githubChart {
-    width: 97%;
-    cursor: pointer;
-    margin-left: 7px;
-    padding: 5px;
-    border: 4px dotted #3ccc64;
-  }
-  .root {
-    h1 {
-      margin: 20px 10px 20px 0;
-      padding-left: 9px;
-      border-left: 6px solid #ea5055;
-      font-weight: 600;
-      line-height: 1.4;
-      color: #222;
-    }
-    p {
-      font-size: 16px;
-    }
-  }
-  .about_contact{
-    display: flex;
-    flex-grow: 1;
-    font-weight: 600;
-    font-size: 18px;
-    a{
-      width: 100%;
-    color: #fff;
-    }
-    .qq,.email,.wechat{
-      box-shadow: 0 0 5px rgba(0,0,0,.1);
-      border-radius: 4px;
-      padding: 25px;
-      margin: 15px 6px ;
-      display: flex;
-      justify-content: center;
-      position: relative;
-    }
-    .qq{
-      background: #bcd4e7;
-    }
-    .email{
-      background: #e66666;
-    }
-    .wechat{
-      background: #3ccc64;
-    }
-    i{
-      position: absolute;
-      font-size: 60px;
-      font-weight: 400;
-      color: #fff;
-      left: 16px;
-      bottom: 6px;
-      opacity: .4;
-      transform: rotate(20deg);
-    }
-  }
-}
+@import './index.less';
 </style>
