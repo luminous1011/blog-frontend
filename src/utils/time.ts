8d6e20ec9e75c2b07eb289e7987a954bb551fe82
panjinshan <pjspan@foxmail.com>
2023-02-17 11:00:03 +0800

    emoji and timejs

--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,10 +1,9 @@
 const ONE_SECOND = 1000;
-const ONE_MINUTE = 60000;
-const ONE_HOUR = 3600000;
-const ONE_DAY = 86400000;
+const ONE_MINUTE = ONE_SECOND * 60;
+const ONE_HOUR = ONE_MINUTE * 60;
+const ONE_DAY = ONE_HOUR * 24;
 const ONE_MONTH = 2592000000;
 const ONE_YEAR = 31104000000;
-
 
 export function timestampToTime(timestamp: number) {
   const date = new Date(timestamp); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
@@ -22,16 +21,26 @@ export function timestampToTime(timestamp: number) {
 }
 
 export function commentFormatTime(timestamp: number) {
-  const now = new Date().getTime() ; //1 675 932 085 812
+  const now = new Date().getTime(); //1 675 932 085 812
+  // 当前零点的时间
+  const zeroDate = now - (now % 86400) - 3600 * 8;
 
   const temp = now - timestamp;
-  if (temp / ONE_SECOND < 60) {
+  console.error(zeroDate);
+  console.error(now);
+  console.error(timestamp);
+  console.error(temp);
+  console.error(now - zeroDate);
+  // if(temp>(timestamp-zeroDate)){
+  //   return "昨天 " + hourMinute(timestamp);
+  // }
+  if (temp / ONE_SECOND < 60 && timestamp > zeroDate) {
     return Math.ceil(temp / ONE_SECOND) + " 秒前";
-  } else if (temp / ONE_MINUTE < 60) {
+  } else if (temp / ONE_MINUTE < 60 && timestamp > zeroDate) {
     return Math.ceil(temp / ONE_MINUTE) + " 分钟前";
-  } else if (temp / ONE_HOUR < 24) {
+  } else if (temp / ONE_HOUR < 24 && timestamp > zeroDate) {
     return Math.floor(temp / ONE_HOUR) + " 小时前";
-  } else if (temp / ONE_HOUR > 24 && temp / ONE_HOUR < 48) {
+  } else if (timestamp > zeroDate - ONE_DAY && timestamp < zeroDate) {
     return "昨天 " + hourMinute(timestamp);
   } else if (temp / ONE_DAY < 7) {
     return Math.floor(temp / ONE_DAY) + " 天前";
@@ -44,10 +53,10 @@ function monthDay(timestamp: number) {
   const date = new Date(timestamp);
 
   const M =
-  (date.getMonth() + 1 < 10
-    ? "" + (date.getMonth() + 1)
-    : date.getMonth() + 1) + "月";
-const D = date.getDate() + "日";
+    (date.getMonth() + 1 < 10
+      ? "" + (date.getMonth() + 1)
+      : date.getMonth() + 1) + "月";
+  const D = date.getDate() + "日";
   return M + D;
 }
 
